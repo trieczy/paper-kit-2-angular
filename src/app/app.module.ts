c0aedabdf046f3bea0f0b954188d3109b3576f85
Trixie Mae Padillo <trixie.padillo@bcstechnology.com.au>
2023-10-04 18:05:10 +0800

    added sending of form on contact us page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { RouterModule } from "@angular/router";
 import { AppRoutingModule } from "./app.routing";
@@ -8,11 +8,28 @@ import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
 
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { NavComponent } from './shared/nav/nav.component';
+import { NavComponent } from "./shared/nav/nav.component";
+import { AboutComponent } from "./components/about/about.component";
+import { LatestNewsComponent } from "./components/latest-news/latest-news.component";
+import { AnnouncementComponent } from "./components/latest-news/announcement/announcement.component";
+import { FeatureArticleComponent } from "./components/latest-news/feature-article/feature-article.component";
+import { MediaComponent } from "./components/latest-news/media/media.component";
+import { ContactUsComponent } from "./components/contact-us/contact-us.component";
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NavComponent,
+    AboutComponent,
+    LatestNewsComponent,
+    AnnouncementComponent,
+    FeatureArticleComponent,
+    MediaComponent,
+    ContactUsComponent,
+  ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     NgbModule,
     FormsModule,
@@ -21,5 +38,6 @@ import { NavComponent } from './shared/nav/nav.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [ContactUsComponent],
 })
 export class AppModule {}
--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
+import "assets/js/smtp.js";
+import { environment } from "environments/environment";
+declare let Email: any;
 @Component({
   selector: "app-contact-us",
   templateUrl: "./contact-us.component.html",
@@ -7,7 +11,59 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ContactUsComponent implements OnInit {
   focus: any;
-  constructor() {}
+  contactUsForm: FormGroup;
+  displayNotif: boolean = false;
+  formIsSending: boolean = false;
+  loading: boolean = true;
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.contactUsForm = this.fb.group({
+      fname: [""],
+      lname: [""],
+      email: [""],
+      phoneNumber: [""],
+      messages: [""],
+    });
+  }
+
+  onSubmit(): void {
+    this.formIsSending = true;
+    (document.getElementById("submit") as HTMLButtonElement).disabled = true;
+    Email.send({
+      Host: environment.hostname,
+      Username: environment.username,
+      Password: environment.password,
+      To: environment.adminemail,
+      From: environment.adminemail,
+      Subject: "Contact Us",
+      Body:
+        "<b>First Name:</b> " +
+        this.contactUsForm.controls.fname.value +
+        "<br />" +
+        "<b>Last Name:</b> " +
+        this.contactUsForm.controls.lname.value +
+        "<br />" +
+        "<b>Email Address:</b> " +
+        this.contactUsForm.controls.email.value +
+        "<br />" +
+        "<b>Phone Number:</b> " +
+        this.contactUsForm.controls.phoneNumber.value +
+        "<br />" +
+        "<b>Message:</b> " +
+        this.contactUsForm.controls.messages.value,
+    }).then((message) => {
+      this.updateNotif();
+      this.formIsSending = false;
+      (document.getElementById("submit") as HTMLButtonElement).disabled = false;
+      this.contactUsForm.reset();
+      setTimeout(() => {
+        this.updateNotif();
+      }, 3000);
+    });
+  }
+
+  updateNotif(): boolean {
+    return (this.displayNotif = !this.displayNotif);
+  }
 }
